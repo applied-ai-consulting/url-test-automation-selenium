00ec2d8e4a1e12b10df705148f4423ed7aadf3eb
pranav@7789 <pranav@appliedaiconsulting.com>
2022-02-10 10:14:34 +0530

    webdriver manager

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
@@ -10,7 +11,11 @@ def pytest_addoption(parser):
 def setup(pytestconfig):
     # chrome_options = webdriver.ChromeOptions()
     # driver = webdriver.Chrome(ChromeDriverManager().download_and_install())
-    s = Service(r"C:\Users\AAIC\Downloads\chromedriver_win32\chromedriver.exe")
-    driver = webdriver.Chrome(service=s)
+    # s = Service(r"C:\Users\AAIC\Downloads\chromedriver_win32\chromedriver.exe")
+    # driver = webdriver.Chrome(service=s)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
     yield {"driver": driver, "url": pytestconfig.getoption("url")}
