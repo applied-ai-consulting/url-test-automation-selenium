50e211686a97a1a83cb3606baf6edcc849af692e
pranav@7789 <pranav@appliedaiconsulting.com>
2022-02-22 10:07:41 +0530

    removed try except

--- test_a.py
+++ test_a.py
@@ -1,18 +1,11 @@
 from datetime import datetime
-import pdb
 
 def test_Openurl(setup):
-    pdb.set_trace()
     driver = setup["driver"]
     url = setup["url"]
-    
-    try:
-        before_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
-        driver.get(url)
-        now_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
-    except Exception as e:
-        print(e.message)
-    
+    before_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
+    driver.get(url)
+    now_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
     assert driver.current_url == url
     driver.save_screenshot("ss.png")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
