5f500632888de5b403ae763bacac150008b2b7fb
Pranav Kakade <pranav@appliedaiconsulting.com>
2022-02-25 13:36:18 +0000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from pytz import timezone
 from datetime import datetime 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 
 def pytest_addoption(parser):
@@ -14,11 +15,11 @@ def setup(pytestconfig):
     # driver = webdriver.Chrome(ChromeDriverManager().download_and_install())
     # s = Service(r"C:\Users\AAIC\Downloads\chromedriver_win32\chromedriver.exe")
     # driver = webdriver.Chrome(service=s)
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chromedriver_path ="/usr/local/bin/chromedriver"
-    driver = webdriver.Chrome(chromedriver_path,chrome_options=chrome_options)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    chromedriver_path =Service("/usr/local/bin/chromedriver")
+    driver = webdriver.Chrome(chromedriver_path,options=options)
     driver.maximize_window()
     yield {"driver": driver, "url": pytestconfig.getoption("url")}
 
