0e555c7b1579090eec47b17b9ed6a6311ab455cd
Pranav Kakade <pranav@appliedaiconsulting.com>
2022-02-27 05:20:38 +0000

    Update test_a.py

--- test_a.py
+++ test_a.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import pytest
 
-
+@pytest.mark.usefixtures("timings")
 def test_Openurl(setup):
 
     driver = setup["driver"]
@@ -10,6 +11,8 @@ def test_Openurl(setup):
     driver.get(url)
     now_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
     response_time = int(now_time) - int(before_time)
+    response_time = int(now_time) - int(before_time)
+    first_url_response_time = timings('first_resp_time', response_time)
     assert driver.current_url == url
     driver.save_screenshot("report/ss.png")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
