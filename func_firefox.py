ede12d5b6e6dcb0beba4b5141acb9244b8f97ecd
Pranav Kakade <pranav@appliedaiconsulting.com>
2022-08-19 06:15:12 +0000

    Update func_firefox.py

--- func_firefox.py
+++ func_firefox.py
@@ -6,7 +6,7 @@ def firefox_webdrvr():
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
-    driver_path = Service(r"/usr/local/bin/geckodriver")
+    driver_path = Service(r"/usr/bin/geckodriver")
     # driver_path =Service(r"C:\Users\AAIC\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe")
     driver = webdriver.Firefox(service=driver_path,options = options)
     return driver
