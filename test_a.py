67ef3e2eae54b5c2af4c0a0b27e3a1b110795250
Pranav Kakade <pranav@appliedaiconsulting.com>
2022-09-15 09:35:19 +0000

    Update test_a.py

--- test_a.py
+++ test_a.py
@@ -11,7 +11,6 @@ def test_Openurl(setup,timings):
     driver.get(url)
     now_time = datetime.now().strftime('%H%M%S%f')  #  Timestamp
     response_time = int(now_time) - int(before_time)
-    response_time = int(now_time) - int(before_time)
     first_url_response_time = timings(response_time)
     assert driver.current_url == url
     driver.save_screenshot("report/ss.png")
