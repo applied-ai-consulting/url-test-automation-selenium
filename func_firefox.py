ef70f08af2bd1a9151662b3681f676fd7ec39cb4
Omkarjodh18 <115140395+Omkarjodh18@users.noreply.github.com>
2023-09-27 11:05:53 +0530

    Update func_firefox.py
    firefox driver path changed

--- func_firefox.py
+++ func_firefox.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 # from selenium.webdriver.firefox.service import Service
@@ -10,7 +11,9 @@ def firefox_webdrvr():
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     # driver_path = Service(r"/usr/bin/geckodriver")
-    ff_driver_path = GeckoDriverManager().install()
+    # ff_driver_path = GeckoDriverManager().install()
+    firefox_driver_path = Service(r"/usr/bin/geckodriver")
     # driver_path =Service(r"C:\Users\AAIC\Downloads\geckodriver-v0.30.0-win64\geckodriver.exe")
-    driver = webdriver.Firefox(executable_path=ff_driver_path,options = options)
+    # driver = webdriver.Firefox(executable_path=ff_driver_path,options = options)
+    driver = webdriver.Firefox(service=firefox_driver_path,options = options)
     return driver
